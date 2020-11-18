61f21bbc9d4d187ef2b1b21c1a865dc6d231fd18
Priyam Suthar <suthar.p@husky.neu.edu>
2020-11-17 23:39:42 -0500

    ASG and ALB

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,9 @@ provider "aws" {
   version = "~> 2.0"
   region     = var.region
 }
-# create the VPC
+###########################
+#         VPC             #
+###########################
 resource "aws_vpc" "My_VPC" {
   cidr_block           = var.vpcCIDRblock
   instance_tenancy     = var.instanceTenancy
@@ -28,54 +30,9 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Create the Security Group
-resource "aws_security_group" "Application_SG"  {
-  # allow ingress of port 22
-  vpc_id = aws_vpc.My_VPC.id
-
-  ingress {
-    cidr_blocks = var.ingressCIDRblock
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = var.ingressCIDRblock
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = var.ingressCIDRblock
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-  }
-
-  # allow egress of all ports
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "${var.vpcName}-${timestamp()} Application - Security Group"
-    Description = "${var.vpcName}-${timestamp()} Security Group"
-  }
-} # end resource
-
-# Create the Internet Gateway
-resource "aws_internet_gateway" "My_VPC_GW" {
-  vpc_id = aws_vpc.My_VPC.id
-  tags = {
-    Name = "${var.vpcName}-${timestamp()} Internet Gateway"
-  }
-} # end resource
-
-# Create the Route Table
+###########################
+#     ROUTE TABLE         #
+###########################
 resource "aws_default_route_table" "My_VPC_route_table" {
   default_route_table_id = aws_vpc.My_VPC.default_route_table_id
   route {
@@ -94,7 +51,81 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_default_route_table.My_VPC_route_table.id
 } # end resource
 
-# Database Security Group
+###########################
+#     LOAD BALANCER SG    #
+###########################
+resource "aws_security_group" "loadBalancer_SG" {
+  name          = "Load Balancer SG (Webapp)"
+  vpc_id        =  aws_vpc.My_VPC.id
+
+  ingress{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks  = var.ingressCIDRblock
+  }
+
+  ingress{
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks  = var.ingressCIDRblock
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = var.egressCIDRblock
+  }
+
+  tags = {
+    Description = "Load Balancer Security Group for WEBAPP project"
+  }
+}
+
+
+###########################
+#     APPLICATION SG      #
+###########################
+resource "aws_security_group" "Application_SG"  {
+  # allow ingress of port 22
+  vpc_id = aws_vpc.My_VPC.id
+
+  ingress {
+    cidr_blocks = var.ingressCIDRblock
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.loadBalancer_SG.id]
+  }
+
+  # allow egress of all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.vpcName}-${timestamp()} Application - Security Group"
+    Description = "${var.vpcName}-${timestamp()} Security Group"
+  }
+} # end resource
+
+###########################
+#     INTERNET GATEWAY    #
+###########################
+resource "aws_internet_gateway" "My_VPC_GW" {
+  vpc_id = aws_vpc.My_VPC.id
+  tags = {
+    Name = "${var.vpcName}-${timestamp()} Internet Gateway"
+  }
+} # end resource
+
+###########################
+#       DATABASE SG       #
+###########################
 resource "aws_security_group" "database_sg" {
   name = "database_sg"
   vpc_id = aws_vpc.My_VPC.id
@@ -414,7 +445,9 @@ resource "aws_iam_role_policy_attachment" "codedeploy-ec2-s3-attach-policy" {
 }
 
 
-# DynamoDB Table
+###################################
+#          DYNAMO DB              #
+###################################
 resource "aws_dynamodb_table" "My_Dynamodb_Table" {
   name           = var.dynamodb_table_name
   billing_mode   = "PROVISIONED"
@@ -430,11 +463,15 @@ resource "aws_dynamodb_table" "My_Dynamodb_Table" {
 }
 
 
-# IAM Instance Profile
+###################################
+#      IAM INSTANCE PROFILE       #
+###################################
 resource "aws_iam_instance_profile" "EC2_S3_Role" {
   name = "EC2_S3_Role"
   role =  aws_iam_role.My_EC2_Role.name
 }
+
+###################################
 
 
 
@@ -522,10 +559,22 @@ resource "aws_codedeploy_app" "code_deploy_app" {
 }
 
 resource "aws_codedeploy_deployment_group" "code_deploy_deployment_group" {
-  app_name              = aws_codedeploy_app.code_deploy_app.name
-  deployment_group_name = "csye6225-webapp-deployment"
-  deployment_config_name = "CodeDeployDefault.AllAtOnce"
-  service_role_arn      = aws_iam_role.CodeDeployServiceRole.arn
+  app_name                = aws_codedeploy_app.code_deploy_app.name
+  deployment_group_name   = "csye6225-webapp-deployment"
+  deployment_config_name  = "CodeDeployDefault.AllAtOnce"
+  service_role_arn        = aws_iam_role.CodeDeployServiceRole.arn
+  autoscaling_groups      = [aws_autoscaling_group.autoscaling_group.name]
+
+  load_balancer_info {
+    target_group_info {
+      name = aws_lb_target_group.alb-target-group.name
+    }
+  }
+
+  alarm_configuration {
+    alarms = ["Deployment-Alarm"]
+    enabled = true
+  }
 
   ec2_tag_filter {
     key   = "WebappServer"
@@ -546,6 +595,9 @@ resource "aws_codedeploy_deployment_group" "code_deploy_deployment_group" {
   depends_on = [aws_codedeploy_app.code_deploy_app]
 }
 
+###################################
+#            ROUTE 53             #
+###################################
 data "aws_route53_zone" "selected" {
   name         = "${var.environment}.${var.domainName}"
 }
@@ -558,4 +610,186 @@ resource "aws_route53_record" "serverRecord" {
   records = [aws_instance.EC2_Instance.public_ip]
   depends_on = [aws_instance.EC2_Instance]
 }
-# end vpc.tf
+
+###################################
+#   ASG LAUNCH CONFIGURATION      #
+###################################
+resource "aws_launch_configuration" "ASG_LaunchConfiguration" {
+  name            = "ASG Launch Configuration (Webapp)"
+  image_id        = data.aws_ami.custom_AMI.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.Application_SG.id]
+  key_name        = var.ssh_key
+  user_data       = templatefile("${path.module}/user_data.sh",
+  {
+    aws_bucket_name = var.bucketName,
+    aws_db_name = aws_db_instance.RDS_Instance.name,
+    aws_db_username = aws_db_instance.RDS_Instance.username,
+    aws_db_password = aws_db_instance.RDS_Instance.password,
+    aws_region = var.region,
+    aws_db_host = aws_db_instance.RDS_Instance.address,
+    aws_app_port = var.appPort
+  })
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.EC2_S3_Role.name
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "20"
+    delete_on_termination = true
+  }
+
+  depends_on = [aws_db_instance.RDS_Instance]
+}
+
+###################################
+#        LB TARGET GROUP          #
+###################################
+resource "aws_lb_target_group" "alb-target-group" {
+
+  name     = "ALB-targetGroup-Webapp"
+  port     = "3000"
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.My_VPC.id
+
+  tags     = {
+    name = "alb-target-group"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    interval            = 30
+    path                = "/healthstatus"
+    port                = "3000"
+    matcher             = "200"
+  }
+}
+
+###################################
+#          LOAD BALANCER          #
+###################################
+resource "aws_lb" "application-loadBalancer" {
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [ aws_security_group.loadBalancer_SG.id ]
+  subnets            = aws_subnet.public.*.id
+  ip_address_type    = "ipv4"
+  tags = {
+    Environment = var.environment
+    Name = "ALB-Webapp"
+  }
+}
+
+resource "aws_lb_listener" "webapp-listener" {
+  load_balancer_arn = aws_lb.application-loadBalancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-target-group.arn
+  }
+}
+
+###################################
+#       ROUTE 53 - ALB            #
+###################################
+data "aws_route53_zone" "selected" {
+  name         = "${var.environment}.${var.domainName}"
+  private_zone = false
+}
+# Alias Record for ALB
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = data.aws_route53_zone.selected.name
+  type    = "A"
+
+  alias {
+    name = aws_lb.application-loadBalancer.dns_name
+    zone_id = aws_lb.application-loadBalancer.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+###################################
+#              ASG                #
+###################################
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name                 = "ASG-Webapp"
+  launch_configuration = aws_launch_configuration.ASG_LaunchConfiguration.name
+  min_size             = 3
+  max_size             = 5
+  default_cooldown     = 60
+  desired_capacity     = 3
+
+  vpc_zone_identifier = aws_subnet.public.*.id
+
+  target_group_arns    = [ aws_lb_target_group.alb-target-group.arn ]
+
+  tag {
+    key                 = "WebappServer"
+    value               = "CI/CD"
+    propagate_at_launch = true
+  }
+}
+
+# ASG POLICIES
+resource "aws_autoscaling_policy" "server-scaleUp-policy" {
+  name                   = "Server-ScaleUp-Policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.autoscaling_group.name
+}
+
+resource "aws_autoscaling_policy" "server-scaleDown-policy" {
+  name                   = "Server-ScaleDown-Policy"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.autoscaling_group.name
+}
+
+## ASG RULES / METRICS TO WATCH
+resource "aws_cloudwatch_metric_alarm" "CPU-High-Alarm" {
+  alarm_name          = "CPU-High-Alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "90"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.autoscaling_group.name
+  }
+  alarm_description = "Scale-up if CPU Utilization > 90% for 1 minute"
+  alarm_actions     = [ aws_autoscaling_policy.server-scaleUp-policy.arn ]
+}
+
+resource "aws_cloudwatch_metric_alarm" "CPU-Low-Alarm" {
+  alarm_name          = "CPU-Low-Alarm"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "70"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.autoscaling_group.name
+  }
+  alarm_description = "Scale-down if CPU Utilization < 70% for 1 minute"
+  alarm_actions     = [ aws_autoscaling_policy.server-scaleDown-policy.arn ]
+}
+
+
+
+
+
+
+
+
+
